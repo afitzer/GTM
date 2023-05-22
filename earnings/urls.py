dc3482cee3e8f14132cde1d6efee8f508aea3ba7
afitzer <alexpfitzgerald@yahoo.com>
2023-05-21 21:49:06 -0500

    re-ordered the paths in the urls.py

--- earnings/urls.py
+++ earnings/urls.py
@@ -5,14 +5,14 @@ app_name = "earnings"
 
 urlpatterns = [
     path('', TripListView.as_view(), name='index'),
+    path('trips/<int:pk>/', TripDetailView.as_view(), name='trip_detail'),
     path('trips/create/', TripCreateView.as_view(), name='trip_create'),
     path('flights/', FlightListView.as_view(), name='flights'),
-    path('hotels/', HotelListView.as_view(), name='hotels'),
-    path('events/', EventListView.as_view(), name='events'),
     path('flights/create/', FlightCreateView.as_view(), name='flight_create'),
+    path('hotels/', HotelListView.as_view(), name='hotels'),
     path('hotels/create/', HotelCreateView.as_view(), name='hotel_create'),
+    path('events/', EventListView.as_view(), name='events'),
     path('events/create/', EventCreateView.as_view(), name='event_create'),
-    path('trips/<int:pk>/', TripDetailView.as_view(), name='trip_detail'),
     path('events/<int:pk>/update/', EventUpdateView.as_view(), name='event_update'),
     path('events/<int:pk>/delete/', EventDeleteView.as_view(), name='event_delete'),
     path('gapminder/', gapminder, name='gapminder'),
